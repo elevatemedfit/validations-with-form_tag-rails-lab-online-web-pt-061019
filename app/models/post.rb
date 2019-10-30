e0fd42b11663d6d704813aa9ce8ba54179f123d7
Chris Junker <elevatemedfit@gmail.com>
2019-10-30 18:32:26 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 250}
-  validates :category, inclusion: { in: %w(Non-Fiction, Fiction)}
+  validates :category, inclusion: { in: %w(Non-Fiction Fiction)}
 end
